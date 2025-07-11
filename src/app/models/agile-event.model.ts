a577aa232f6a0a6cc2478e5f7b2d3daed2ecd5c8
mdholder74 <mdholder74@gmail.com>
2025-07-11 11:51:00 -0400

    fix: update related descriptions in JSON and HTML files and styling in agile-section

--- src/app/models/agile-event.model.ts
+++ src/app/models/agile-event.model.ts
@@ -1,5 +1,5 @@
 export interface AgileEvent {
     id: number;
-    type: string; // e.g., "sprint", "release", "planning", "review"
+    title: string; // e.g., "sprint", "release", "planning", "review"
     items: string[]; // List of items related to the event
 }
